4c1435b9a97e01cecef199ebf0e6a7dcd012a583
HairongWu <whr1999@gmail.com>
2018-11-29 10:37:58 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import AddTodo from './components/AddTodo/';
+import ListTask from './components/ListTask/';
+import ClearTask from './components/ClearTask';
+import MarkAllTask from './components/MarkAllTask';
+import  {addTodo, toggleTodo, clearTask, markAllTask} from './action';
 
-//import ProductTable from './components/ProductTable/'
-//import SearchBar from './components/SearchBar/'
-import AddTodo from './components/AddTodo/'
 
 class App extends Component {
   render() {
+    var listA=this.props.state;
     return (
       <div>
-        
-        <AddTodo/>
+        <AddTodo addTodo={this.props.addtodo}/>
+        <MarkAllTask Click={this.props.markalltaskA}/>
+        <ClearTask Click={this.props.cleartaskA}/>
+        <ListTask list={listA} ToDoClick={this.props.toggleToDo}/>
       </div>
     );
   }
 }
 
-export default App;
 
 
-//<SearchBar />
-//<ProductTable />
+const mapStateToProps = state => {
+  return {
+    state: state,
+  };
+};
+
+
+function mapDispatchToProps(dispatch) {
+  return({
+    addtodo: (text)=>dispatch(addTodo(text)),
+    toggleToDo: (id) => dispatch(toggleTodo(id)),
+    cleartaskA: () => dispatch(clearTask()),
+    markalltaskA: ()=> dispatch(markAllTask()),
+  })
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
